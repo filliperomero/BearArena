59cc73c8bcbdac38fcd36f456f578a4792689894
filliperomero <fillipe.romero@gmail.com>
2025-09-28 22:16:08 -0600

    Add Player Controller

---
+++ Source/BearArena/Private/Player/BA_PlayerController.cpp
@@ -0,0 +1,71 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "Player/BA_PlayerController.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+#include "GameFramework/Character.h"
+
+void ABA_PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputLocalPlayerSubsystem* InputSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+
+	if (IsValid(InputSubsystem))
+	{
+		for (UInputMappingContext* Context : InputMappingContexts)
+		{
+			InputSubsystem->AddMappingContext(Context, 0);
+		}
+	}
+
+	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent);
+	if (IsValid(EnhancedInputComponent))
+	{
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ThisClass::Jump);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ThisClass::StopJumping);
+
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::Move);
+		
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ThisClass::Look);
+	}
+}
+
+void ABA_PlayerController::Jump()
+{
+	if (!IsValid(GetCharacter())) return;
+	
+	GetCharacter()->Jump();
+}
+
+void ABA_PlayerController::StopJumping()
+{
+	if (!IsValid(GetCharacter())) return;
+
+	GetCharacter()->StopJumping();
+}
+
+void ABA_PlayerController::Move(const FInputActionValue& Value)
+{
+	if (!IsValid(GetPawn())) return;
+
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+
+	// Find which way is forward
+	const FRotator YawRotation(FRotator(0.f, GetControlRotation().Yaw, 0.f));
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	
+	GetPawn()->AddMovementInput(ForwardDirection, MovementVector.Y);
+	GetPawn()->AddMovementInput(RightDirection, MovementVector.X);
+}
+
+void ABA_PlayerController::Look(const FInputActionValue& Value)
+{
+	const FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	AddYawInput(LookAxisVector.X);
+	AddPitchInput(LookAxisVector.Y);
+}
---
+++ Source/BearArena/Public/Player/BA_PlayerController.h
@@ -0,0 +1,38 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BA_PlayerController.generated.h"
+
+struct FInputActionValue;
+class UInputAction;
+class UInputMappingContext;
+
+UCLASS()
+class BEARARENA_API ABA_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void SetupInputComponent() override;
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "BearArena|Input")
+	TArray<TObjectPtr<UInputMappingContext>> InputMappingContexts;
+
+	UPROPERTY(EditDefaultsOnly, Category = "BearArena|Input")
+	TObjectPtr<UInputAction> JumpAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "BearArena|Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "BearArena|Input")
+	TObjectPtr<UInputAction> LookAction;
+
+	void Jump();
+	void StopJumping();
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+};
