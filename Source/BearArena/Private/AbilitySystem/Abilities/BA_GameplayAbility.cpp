9923a37ef2d99445c31fa8d96d07ccf07aa83413
filliperomero <fillipe.romero@gmail.com>
2025-10-01 23:21:50 -0600

    Add C++ Class for all abilities to inherit from

---
+++ Source/BearArena/Private/AbilitySystem/Abilities/BA_GameplayAbility.cpp
@@ -0,0 +1,3 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "AbilitySystem/Abilities/BA_GameplayAbility.h"
---
+++ Source/BearArena/Public/AbilitySystem/Abilities/BA_GameplayAbility.h
@@ -0,0 +1,17 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "BA_GameplayAbility.generated.h"
+
+UCLASS()
+class BEARARENA_API UBA_GameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "BearArena|Debug")
+	bool bDrawDebugs { false };
+};
