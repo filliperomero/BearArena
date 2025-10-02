7295ba0de3a1f2479ba8d7b5b747b327a7a1597a
filliperomero <fillipe.romero@gmail.com>
2025-10-01 22:57:41 -0600

    Add custom gameplay tags

---
+++ Source/BearArena/Private/GameplayTags/BA_Tags.cpp
@@ -0,0 +1,11 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "GameplayTags/BA_Tags.h"
+
+namespace BATags
+{
+	namespace Abilities
+	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(Primary, "BATags.Abilities.Primary", "Tag for the Primary Ability");
+	}
+}
---
+++ Source/BearArena/Public/GameplayTags/BA_Tags.h
@@ -0,0 +1,14 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NativeGameplayTags.h"
+
+namespace BATags
+{
+	namespace Abilities
+	{
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(Primary);
+	}
+}
