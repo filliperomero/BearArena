35191df9da73f8de3c324c0995cab4d046096bc7
filliperomero <fillipe.romero@gmail.com>
2025-10-02 23:21:13 -0600

    Add Melee and Range Enemy (basic Character)

---
+++ Source/BearArena/Private/Characters/BA_EnemyCharacter.cpp
@@ -0,0 +1,33 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "Characters/BA_EnemyCharacter.h"
+
+#include "AbilitySystemComponent.h"
+
+ABA_EnemyCharacter::ABA_EnemyCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+}
+
+UAbilitySystemComponent* ABA_EnemyCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+void ABA_EnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (!IsValid(GetAbilitySystemComponent())) return;
+
+	GetAbilitySystemComponent()->InitAbilityActorInfo(this, this);
+
+	if (!HasAuthority()) return;
+
+	GiveStartupAbilities();
+}
+
---
+++ Source/BearArena/Public/Characters/BA_EnemyCharacter.h
@@ -0,0 +1,26 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BA_BaseCharacter.h"
+#include "BA_EnemyCharacter.generated.h"
+
+class UAbilitySystemComponent;
+
+UCLASS()
+class BEARARENA_API ABA_EnemyCharacter : public ABA_BaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+	ABA_EnemyCharacter();
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+};
