03837f53fe9a8ba003c8de2c3b28d665f986d8a6
filliperomero <fillipe.romero@gmail.com>
2025-10-04 14:52:01 -0600

    Add custom class for Primary Ability and add custom overlap function to check overlapped actors

---
+++ Source/BearArena/Public/AbilitySystem/Abilities/Player/BA_Primary.h
@@ -0,0 +1,27 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/BA_GameplayAbility.h"
+#include "BA_Primary.generated.h"
+
+UCLASS()
+class BEARARENA_API UBA_Primary : public UBA_GameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "BearArena|Abilities")
+	void HitBoxOverlapTest();
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "BearArena|Abilities")
+	float HitBoxRadius { 100.f };
+
+	UPROPERTY(EditDefaultsOnly, Category = "BearArena|Abilities")
+	float HitBoxForwardOffset { 200.f };
+
+	UPROPERTY(EditDefaultsOnly, Category = "BearArena|Abilities")
+	float HitBoxElevationOffset { 20.f };
+};
