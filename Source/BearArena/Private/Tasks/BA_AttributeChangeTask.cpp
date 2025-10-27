ef54393f3d53b34ac5808060c1da4ee39ed44e03
filliperomero <fillipe.romero@gmail.com>
2025-10-26 21:57:32 -0600

    Add Attribute Change Task so we can listen to changes in a specific attribute

---
+++ Source/BearArena/Private/Tasks/BA_AttributeChangeTask.cpp
@@ -0,0 +1,37 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "Tasks/BA_AttributeChangeTask.h"
+#include "AbilitySystemComponent.h"
+
+UBA_AttributeChangeTask* UBA_AttributeChangeTask::ListenForAttributeChange(UAbilitySystemComponent* AbilitySystemComponent, FGameplayAttribute Attribute)
+{
+	UBA_AttributeChangeTask* WaitForAttributeChangeTask = NewObject<UBA_AttributeChangeTask>();
+	WaitForAttributeChangeTask->ASC = AbilitySystemComponent;
+	WaitForAttributeChangeTask->AttributeToListFor = Attribute;
+
+	if (!IsValid(AbilitySystemComponent))
+	{
+		WaitForAttributeChangeTask->RemoveFromRoot();
+		return nullptr;
+	}
+
+	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(Attribute).AddUObject(WaitForAttributeChangeTask, &UBA_AttributeChangeTask::AttributeChanged);
+
+	return WaitForAttributeChangeTask;
+}
+
+void UBA_AttributeChangeTask::EndTask()
+{
+	if (ASC.IsValid())
+	{
+		ASC->GetGameplayAttributeValueChangeDelegate(AttributeToListFor).RemoveAll(this);
+	}
+
+	SetReadyToDestroy();
+	MarkAsGarbage();
+}
+
+void UBA_AttributeChangeTask::AttributeChanged(const FOnAttributeChangeData& Data)
+{
+	OnAttributeChanged.Broadcast(Data.Attribute, Data.NewValue, Data.OldValue);
+}
---
+++ Source/BearArena/Public/Tasks/BA_AttributeChangeTask.h
@@ -0,0 +1,33 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "BA_AttributeChangeTask.generated.h"
+
+struct FOnAttributeChangeData;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnAttributeChanged, FGameplayAttribute, Attribute, float, NewValue,
+                                               float, OldValue);
+
+UCLASS()
+class BEARARENA_API UBA_AttributeChangeTask : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOnAttributeChanged OnAttributeChanged;
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"))
+	static UBA_AttributeChangeTask* ListenForAttributeChange(UAbilitySystemComponent* AbilitySystemComponent, FGameplayAttribute Attribute);
+
+	UFUNCTION(BlueprintCallable)
+	void EndTask();
+
+	void AttributeChanged(const FOnAttributeChangeData& Data);
+
+	TWeakObjectPtr<UAbilitySystemComponent> ASC;
+	FGameplayAttribute AttributeToListFor;
+};
