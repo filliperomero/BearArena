0e8aa314910e8299d47cc41782ad1401a5678c1f
filliperomero <fillipe.romero@gmail.com>
2025-11-23 23:32:49 -0700

    Add function to blueprint library to find closest actor with specific tag

--- Source/BearArena/Private/Utils/BA_BlueprintLibrary.cpp
+++ Source/BearArena/Private/Utils/BA_BlueprintLibrary.cpp
@@ -2,6 +2,9 @@
 
 #include "Utils/BA_BlueprintLibrary.h"
 
+#include "Characters/BA_BaseCharacter.h"
+#include "Kismet/GameplayStatics.h"
+
 EHitDirection UBA_BlueprintLibrary::GetHitDirection(const FVector& TargetForward, const FVector& ToInstigator)
 {
 	const float DotProduct = FVector::DotProduct(TargetForward, ToInstigator);
@@ -30,4 +33,35 @@ FName UBA_BlueprintLibrary::GetHitDirectionName(const EHitDirection& HitDirectio
 		case EHitDirection::Back: return FName("Back");
 		default: return FName("None");
 	}
+}
+
+FClosestActorWithTagResult UBA_BlueprintLibrary::FindClosestActorWithTag(const UObject* WorldContextObject, const FVector& Origin, const FName& Tag)
+{
+	TArray<AActor*> OutActors;
+	UGameplayStatics::GetAllActorsWithTag(WorldContextObject, Tag, OutActors);
+
+	float ClosestDistance = TNumericLimits<float>::Max();
+	AActor* ClosestActor = nullptr;
+
+	for (AActor* Actor : OutActors)
+	{
+		if (!IsValid(Actor)) continue;
+
+		ABA_BaseCharacter* BaseCharacter = Cast<ABA_BaseCharacter>(Actor);
+		if (!IsValid(BaseCharacter) || !BaseCharacter->IsAlive()) continue;
+
+		const float Distance = FVector::Dist(Origin, BaseCharacter->GetActorLocation());
+
+		if (Distance < ClosestDistance)
+		{
+			ClosestDistance = Distance; 
+			ClosestActor = Actor;
+		}
+	}
+
+	FClosestActorWithTagResult Result;
+	Result.Actor = ClosestActor;
+	Result.Distance = ClosestDistance;
+	
+	return Result;
 }
