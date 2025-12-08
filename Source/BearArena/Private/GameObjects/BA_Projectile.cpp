45c793fb8dd03f53c1e72bc34810e2719fe9c622
filliperomero <fillipe.romero@gmail.com>
2025-12-07 19:24:23 -0700

    Add projectile class/bp and use it in ranged attack

---
+++ Source/BearArena/Private/GameObjects/BA_Projectile.cpp
@@ -0,0 +1,37 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "GameObjects/BA_Projectile.h"
+
+#include "AbilitySystemComponent.h"
+#include "Characters/BA_PlayerCharacter.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+ABA_Projectile::ABA_Projectile()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>("ProjectileMovement");
+	
+	bReplicates = true;
+}
+
+void ABA_Projectile::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+	
+	ABA_PlayerCharacter* PlayerCharacter = Cast<ABA_PlayerCharacter>(OtherActor);
+	if (!IsValid(PlayerCharacter) || !PlayerCharacter->IsAlive()) return;
+	
+	UAbilitySystemComponent* AbilitySystemComponent = PlayerCharacter->GetAbilitySystemComponent();
+	if (!IsValid(AbilitySystemComponent) || !HasAuthority()) return;
+	
+	FGameplayEffectContextHandle ContextHandle = AbilitySystemComponent->MakeEffectContext();
+	FGameplayEffectSpecHandle SpecHandle = AbilitySystemComponent->MakeOutgoingSpec(DamageEffect, 1.f, ContextHandle);
+	
+	// TODO: Use the Damage variable for the amount of damage to cause.
+	
+	AbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+	
+	SpawnImpactEffects();
+	Destroy();
+}
---
+++ Source/BearArena/Public/GameObjects/BA_Projectile.h
@@ -0,0 +1,33 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BA_Projectile.generated.h"
+
+class UGameplayEffect;
+class UProjectileMovementComponent;
+
+UCLASS()
+class BEARARENA_API ABA_Projectile : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ABA_Projectile();
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "BearArena|Damage", meta = (ExposeOnSpawn, ClampMin = "0.0"))
+	float Damage { 10.f };
+	
+	UFUNCTION(BlueprintImplementableEvent, Category = "BearArena|Projectile")
+	void SpawnImpactEffects();
+	
+private:
+	UPROPERTY(VisibleAnywhere, Category = "BearArena|Projectile")
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovement;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "BearArena|Damage")
+	TSubclassOf<UGameplayEffect> DamageEffect;
+};
