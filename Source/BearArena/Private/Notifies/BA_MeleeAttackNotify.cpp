ae8a5c7068e99bcdcba7c73fa6b2f5dfb48b6235
filliperomero <fillipe.romero@gmail.com>
2025-12-13 23:57:03 -0700

    Add Notify State for Melee Attack

---
+++ Source/BearArena/Private/Notifies/BA_MeleeAttackNotify.cpp
@@ -0,0 +1,75 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#include "Notifies/BA_MeleeAttackNotify.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+#include "KismetTraceUtils.h"
+#include "Characters/BA_PlayerCharacter.h"
+#include "GameplayTags/BA_Tags.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UBA_MeleeAttackNotify::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyTick(MeshComp, Animation, FrameDeltaTime, EventReference);
+	
+	if (!IsValid(MeshComp) || !IsValid(MeshComp->GetOwner())) return;
+	
+	TArray<FHitResult> Hits = PerformSphereTrace(MeshComp);
+	SendEventsToActors(MeshComp, Hits);
+}
+
+TArray<FHitResult> UBA_MeleeAttackNotify::PerformSphereTrace(USkeletalMeshComponent* MeshComp) const
+{
+	TArray<FHitResult> OutHits;
+	
+	const FTransform SocketTransform = MeshComp->GetSocketTransform(SocketName);
+	const FVector Start = SocketTransform.GetLocation();
+	const FVector ExtendedSocketDirection = UKismetMathLibrary::GetForwardVector(SocketTransform.GetRotation().Rotator()) * SocketExtensionOffset;
+	const FVector End = Start - ExtendedSocketDirection;
+	
+	TArray<AActor*> ActorsToIgnore;
+	ActorsToIgnore.Add(MeshComp->GetOwner());
+	
+	FCollisionQueryParams Params;
+	Params.AddIgnoredActors(ActorsToIgnore);
+	
+	FCollisionResponseParams ResponseParams;
+	ResponseParams.CollisionResponse.SetAllChannels(ECR_Ignore);
+	ResponseParams.CollisionResponse.SetResponse(ECC_Pawn, ECR_Block);
+	
+	UWorld* World = GEngine->GetWorldFromContextObject(MeshComp, EGetWorldErrorMode::LogAndReturnNull);
+	if (!IsValid(World)) return OutHits;
+	
+	const bool bHit = World->SweepMultiByChannel(OutHits, Start, End, FQuat::Identity, ECC_Visibility, FCollisionShape::MakeSphere(SphereTraceRadius), Params, ResponseParams);
+	
+	if (bDrawDebugs)
+	{
+		DrawDebugSphereTraceMulti(World, Start, End, SphereTraceRadius, EDrawDebugTrace::ForDuration, bHit, OutHits, FColor::Green, FColor::Red, 5.f);
+	}
+	
+	return OutHits;
+}
+
+void UBA_MeleeAttackNotify::SendEventsToActors(USkeletalMeshComponent* MeshComp, const TArray<FHitResult>& Hits) const
+{
+	for (const FHitResult& Hit : Hits)
+	{
+		ABA_PlayerCharacter* PlayerCharacter = Cast<ABA_PlayerCharacter>(Hit.GetActor());
+		if (!IsValid(PlayerCharacter) || !PlayerCharacter->IsAlive()) continue;
+		
+		UAbilitySystemComponent* ASC = PlayerCharacter->GetAbilitySystemComponent();
+		if (!IsValid(ASC)) continue;
+		
+		FGameplayEffectContextHandle ContextHandle = ASC->MakeEffectContext();
+		ContextHandle.AddHitResult(Hit);
+		
+		FGameplayEventData Payload;
+		Payload.Target = PlayerCharacter;
+		Payload.ContextHandle = ContextHandle;
+		Payload.Instigator = MeshComp->GetOwner();
+		
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(MeshComp->GetOwner(), BATags::Events::Enemy::MeleeTraceHit, Payload);
+	}
+}
---
+++ Source/BearArena/Public/Notifies/BA_MeleeAttackNotify.h
@@ -0,0 +1,32 @@
+﻿// Copyright Fillipe Romero. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "BA_MeleeAttackNotify.generated.h"
+
+UCLASS()
+class BEARARENA_API UBA_MeleeAttackNotify : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference) override;
+	
+private:
+	UPROPERTY(EditAnywhere, Category = "BearArena|Debugs")
+	bool bDrawDebugs { true };
+	
+	UPROPERTY(EditAnywhere, Category = "BearArena|Socket")
+	FName SocketName { "FX_Trail_01_R" };
+	
+	UPROPERTY(EditAnywhere, Category = "BearArena|Socket")
+	float SocketExtensionOffset { 40.f };
+	
+	UPROPERTY(EditAnywhere, Category = "BearArena|Socket")
+	float SphereTraceRadius { 60.f };
+	
+	TArray<FHitResult> PerformSphereTrace(USkeletalMeshComponent* MeshComp) const;
+	void SendEventsToActors(USkeletalMeshComponent* MeshComp, const TArray<FHitResult>& Hits) const;
+};
